1660d3c38ec372bfb77af55c55c330fd0aa0aaac
jochimin88 <201905041@p.uapa.edu.do>
2021-11-25 09:20:35 -0400

    updated

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from routes.contacts import contacts
 from flask_sqlalchemy import SQLAlchemy
-
+from config import DATABASE_URI
 
 
 app = Flask(__name__)
@@ -9,9 +9,7 @@ app = Flask(__name__)
 
 app.secret_key = "jochimin"
 
-password = 'G2s~Dj{kK#7Ug>6rD{L'
-
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://root:{password}@localhost/contacts'
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 from app import app
 from utils.db import db
-
+import config
 # this creates the database and the db table from the models
 with app.app_context():
     db.create_all()
